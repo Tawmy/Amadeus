09bfc3e6bb91ccd8631ee0758e17fe0bd0ec5fcd
Tawmy <mail@tawmy.dev>
2022-03-01 21:10:07 +0100

    Remove old rolesmsg command

--- Amadeus.Bot/Modules/RolesModule.cs
+++ Amadeus.Bot/Modules/RolesModule.cs
@@ -9,20 +9,6 @@ namespace Amadeus.Bot.Modules;
 
 public class RolesModule : ApplicationCommandModule
 {
-    [SlashCommand("rolesMsg", "Sends message with button for self-assigning roles.")]
-    [ModeratorSlash]
-    [SlashRequireBotPermissions(Permissions.SendMessages)]
-    public async Task SlashRolesMsg(InteractionContext ctx,
-        [Option("Title", "Title of the message")]
-        string title,
-        [Option("Description", "Description of the message")]
-        string? description = null,
-        [Option("Channel", "Channel to send the message in")]
-        DiscordChannel? channel = null)
-    {
-        await RolesMsgCommand.RunSlash(ctx, title, description, channel);
-    }
-
     [SlashCommand("postMenu", "Pick a self-assign menu to post in given channel.")]
     [ModeratorSlash]
     [SlashRequireBotPermissions(Permissions.SendMessages)]
